1008f1acfc02656ff307022536bf15a9f577cc44
ali <ali.7@outlook.com>
2019-08-24 11:46:39 +0300

    handle-404

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,5 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("ali");
+});
 
+router.use((req, res) => {
+  res.status(404).send("404!");
+});
 module.exports = router;
